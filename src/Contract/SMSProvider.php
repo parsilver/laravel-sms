c4d467b8bb4bc0c4167f9bb23e4cddbb746750cb
parsilver <magician_canobi@hotmail.com>
2019-11-19 10:43:51 +0700

    - Add travis - Support laravel 6+

--- src/Contract/SMSProvider.php
+++ src/Contract/SMSProvider.php
@@ -5,6 +5,7 @@ interface SMSProvider extends SMSProviderFactory
     /**
      * @param string $phoneNumber
      * @param string $message
+     * @return mixed
      */
     public function send($phoneNumber, $message);
 }
